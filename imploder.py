34d925d1198406def23f40e5b472dcaf247a8122
eabderh <eabderh@clemson.edu>
2016-10-22 19:36:29 +0000

    completel reworked imploder to make it simpler
    changed from class to single function that reloads all modules in
    sys.modules that have certain constants set.

--- imploder.py
+++ imploder.py
@@ -1,46 +1,21 @@
 
+__RELOAD__ = True
+__EXPORT__ = True
 
 import imp
-from types import ModuleType
-from inspect import stack
+import sys
 import export
+def implode():
+    for module in sys.modules.values():
+        if '__RELOAD__' in module.__dict__:
+            print('RELOAD - ' + module.__name__)
+            imp.reload(module)
+        if '__EXPORT__' in module.__dict__:
+            print('EXPORT - ' + module.__name__)
+            export.top.module(module)
 
-class Implode:
-    modules = []
+export.top.val('implode', implode)
 
-    # 'public'
-    def __call__(self):
-        for module in self.modules:
-            print('reloading module: ' + module.__name__)
-            print('dependancies:')
-            self.reload(module)
-    def add(self, module):
-        self.modules.append(module)
-    def extend(self, modules):
-        self.modules.extend(modules)
-    def get(self):
-        return self.modules
-
-    # 'private'
-    def reload(self, module):
-        self.rreload(module)
-        export.top.module(module)
-    def rreload(self, module):
-        if '__IMPLODE__' not in module.__dict__:
-            return
-#        if '__file__' not in module.__dict__:
-#            return
-#        if module.__file__.startswith('/usr/'):
-#            return
-        for key, val in module.__dict__.items():
-            if type(val) is ModuleType:
-                self.rreload(val)
-        imp.reload(module)
-        print(' - ' + module.__name__)
-
-_inst = Implode()
-exports = {'implode': _inst}
-export.top.dict(exports)
 
 
 
