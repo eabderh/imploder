7327ba578a7f4940b0bbe6637a7d67ce6deba319
eabderh <eabderh@clemson.edu>
2016-10-26 04:52:10 +0000

    reorganized implode function
    there should be no change in functionality as modification was purely
    aesthetical.

--- imploder.py
+++ imploder.py
@@ -1,18 +1,24 @@
 
-__RELOAD__ = True
-__EXPORT__ = True
 
 import imp
 import sys
 import export
 def implode():
     for module in sys.modules.values():
-        if '__RELOAD__' in module.__dict__:
-            print('RELOAD - ' + module.__name__)
+        items = module.__dict__.items()
+        if ('__IMPLODE__', True) in items:
+            print('IMPLODE - ' + module.__name__)
             imp.reload(module)
-        if '__EXPORT__' in module.__dict__:
-            print('EXPORT - ' + module.__name__)
             export.top.module(module)
+        # @Optional
+        else:
+            if ('__RELOAD__',  True) in items:
+                print('RELOAD - ' + module.__name__)
+                imp.reload(module)
+            if ('__EXPORT__',  True) in items:
+                print('EXPORT - ' + module.__name__)
+                export.top.module(module)
+        # @EndOptional
 
 export.top.val('implode', implode)
 
